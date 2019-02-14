d3066af405494047dfe170d709fd020cdc46a293
Rolf Zambon <led479.rs@gmail.com>
2019-02-13 22:29:09 -0200

    Corrige msg de erro

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var express = require('express'),
 
 // mongoose instance connection url connection
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/Tododb');
+mongoose.connect('mongodb://localhost/Tododb', { useNewUrlParser: true });
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
