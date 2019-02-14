66b8a6d556fb2c7ec734b6cbc8a6fabd0428e16e
Rolf Zambon <led479.rs@gmail.com>
2019-02-14 00:07:43 -0200

    Remove erro de deprecation

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ var express = require('express'),
 
 // mongoose instance connection url connection
 mongoose.Promise = global.Promise;
+mongoose.set('useCreateIndex', true);
 mongoose.connect('mongodb://localhost/Tododb', { useNewUrlParser: true });
 
 
