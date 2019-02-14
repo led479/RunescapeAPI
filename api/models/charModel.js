06f81576943f8e865ee4fe13506941a08bb307bb
Mateus Pandidno <yardbs@gmail.com>
2019-02-13 23:31:44 -0200

    Add Atributos

--- api/models/charModel.js
+++ api/models/charModel.js
@@ -23,11 +23,79 @@ var CharSchema = new Schema({
             type: Number,
             default: 1
         },
-        wc: {
+        rgd: {
+            type: Number,
+            default: 1
+        },
+        pry: {
+            type: Number,
+            default: 1
+        },
+        mgc: {
             type: Number,
             default: 1
         },
         rc: {
+            type: Number,
+            default: 1
+        },
+        hp: {
+            type: Number,
+            default: 1
+        },
+        crf: {
+            type: Number,
+            default: 1
+        },
+        mng: {
+            type: Number,
+            default: 1
+        },
+        fsh: {
+            type: Number,
+            default: 1
+        },
+        ckg: {
+            type: Number,
+            default: 1
+        },
+        wc: {
+            type: Number,
+            default: 1
+        },
+        fm: {
+            type: Number,
+            default: 1
+        },
+        ag: {
+            type: Number,
+            default: 1
+        },
+        hrb: {
+            type: Number,
+            default: 1
+        },
+        thf: {
+            type: Number,
+            default: 1
+        },
+        flc: {
+            type: Number,
+            default: 1
+        },
+        slr: {
+            type: Number,
+            default: 1
+        },
+        fmg: {
+            type: Number,
+            default: 1
+        },
+        crs: {
+            type: Number,
+            default: 1
+        },
+        hun: {
             type: Number,
             default: 1
         }
