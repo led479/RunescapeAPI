829700ce22f58c574624ad0ee51cc73dba0ea176
Rolf Zambon <led479.rs@gmail.com>
2019-02-15 23:23:43 -0200

    Atualiza Char Details

--- client/src/components/char/CharDetails.vue
+++ client/src/components/char/CharDetails.vue
@@ -1,12 +1,15 @@
 <template>
-  <div class="char_stats">
-    <h2 id="name">{{ char.login }}</h2>
-    <div class="char_skills">
-      <span v-for="(skillStats, skillName) in char.stats" :key="skillName">
-        {{ skillName }}: {{skillStats.level}}
-        <br>
-      </span>
-    </div>
+  <div>
+    <b-card class="card_char">
+      <h2 id="name">{{ char.login }}</h2>
+      <div class="char_skills">
+        <span v-for="(skillStats, skillName) in char.stats" :key="skillName">
+          <h4>{{ capitalize(skillName) }}</h4>
+          <ul>Level: {{ skillStats.level }}</ul>
+          <ul>XP: {{ numberWithCommas(skillStats.experience) }}</ul>
+        </span>
+      </div>
+    </b-card>
     <br>
   </div>
 </template>
@@ -18,14 +21,24 @@ export default {
     char: {
       type: Object
     }
+  },
+  methods: {
+    capitalize(string) {
+      return string[0].toUpperCase() + string.slice(1);
+    },
+    numberWithCommas(x) {
+      return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    }
   }
 };
 </script>
 
 <style>
+.card_char{
+  background: linear-gradient(to right, rgba(135, 2, 1, 0), rgba(181, 16, 9, 0.16));
+}
 .char_skills {
   column-count: 8;
-  background: linear-gradient(to right, #ce170a42, #ff07074d);
   color: black;
   padding: 20px;
   flex-wrap: wrap;
@@ -34,11 +47,16 @@ export default {
   font-weight: lighter;
 }
 
+.char_skills ul{
+   padding: 0;
+   margin-bottom: 10px;
+}
+
 #name {
   justify-self: flex-start;
   text-decoration: none;
   display: flex;
-  justify-content: center;
+  justify-content: left;
   align-items: center;
 }
 </style>
