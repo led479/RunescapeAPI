c8ffc03db1c15e9374ef4bab31715e96e6fdf1bb
Rolf Zambon <led479.rs@gmail.com>
2019-02-19 21:27:08 -0300

    Faz funcionar rota

--- api/controllers/charController.js
+++ api/controllers/charController.js
@@ -75,6 +75,6 @@ exports.import_char_2 = function(req, res) {
     let url = 'https://secure.runescape.com/m=hiscore_oldschool/hiscorepersonal.ws?user1=' + req.params.login;
     request(url, function(error, response) {
         const $ = cheerio.load(response.body)
-        res.send($.toString());
+        res.send($.text());
     })
 };
